9f690d3c502f74a91672e10a145d9b2c0b6085b8
coscialp <corentin.sciapi@gmail.com>
2024-03-15 11:16:21 +0100

    Add pylint mypy and black

--- ptv.py
+++ ptv.py
@@ -1,29 +1,41 @@
+""" Command line interface for the project.
+
+This module provides a command line interface for the project. It uses the `click` library to define
+the commands and options.
+
+Example:
+    $ python ptv.py preprocess
+    $ python ptv.py train
+    $ python ptv.py predict
+"""
+
 import click
 
+
 CONTEXT_SETTINGS = {
-    "help_option_names": ['-h', '--help'],
+    "help_option_names": ["-h", "--help"],
 }
 
 
 @click.group(context_settings=CONTEXT_SETTINGS)
 def cli():
-    pass
+    """Command line interface for the project."""
 
 
 @cli.command()
 def preprocess():
-    pass
+    """Preprocess the data."""
 
 
 @cli.command()
 def train():
-    pass
+    """Train the model."""
 
 
 @cli.command()
 def predict():
-    pass
+    """Predict the target variable."""
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     cli()
